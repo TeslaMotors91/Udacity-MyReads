d859c9890aa51863b9d397d0b701d392d42f8651
Mario Bermudez Jr <Mario.Bermudez.Jr@outlook.com>
2018-01-13 16:16:31 -0800

    type: feat body: Implemented routing using react-router-dom with Route, Link and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+// Package Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+// Local Imports
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
